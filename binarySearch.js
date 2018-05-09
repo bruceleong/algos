949dc6871fbfedfbb2136be9cfdd1dd4287043dd
Bruce Leong <brucewleong@gmail.com>
2018-05-09 16:28:29 -0400

    added binary search

---
+++ binarySearch.js
@@ -0,0 +1,31 @@
+/*
+
+binary search
+
+write a function
+*/
+
+let array = [0, 1, 21, 33, 45, 59, 61, 71, 72]
+let targetSum = 33
+
+//answer = 3
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let midpoint = Math.floor((left + right) / 2)
+    console.log(array[midpoint])
+    if (target === array[midpoint]) {
+      return midpoint
+    } else if (target > array[midpoint]) {
+      left = midpoint
+    } else if (target < array[midpoint]) {
+      right = midpoint
+    }
+  }
+  return -1
+}
+
+console.log(binarySearch(array, targetSum))
