eb2ea1766011165084cf7ac73638328b02519a94
Bruce Leong <brucewleong@gmail.com>
2018-05-11 23:12:37 -0400

    updated building block

--- buildingBlocks.js
+++ buildingBlocks.js
@@ -1,10 +1,11 @@
-//reverse an array in place
+{/* Building block techniques to solve all problems*/}
 
+//reverse an array in place
+//O(n) time / space
 function arraySwap(array) {
   let beginningIndex = 0,
       endingIndex = array.length - 1
   while (beginningIndex < endingIndex) {
-
     [array[beginningIndex], array[endingIndex]] = [array[endingIndex], array[beginningIndex]]
     beginningIndex++
     endingIndex--
