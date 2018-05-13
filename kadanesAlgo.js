70c298a545640f59290067a46f842c7cd01a6674
Bruce Leong <brucewleong@gmail.com>
2018-05-13 17:05:34 -0400

    added kadanes algo

---
+++ kadanesAlgo.js
@@ -0,0 +1,22 @@
+//kadanesAlgo
+
+//1 implementation
+function maxSub(array) {
+  if (array === null) return 0
+  let max = 0, maxSum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    console.log(max, 'max', maxSum, 'maxSum')
+    max = Math.max(0, max + array[i])
+    console.log('max after asignment', max, 'array val', array[i])
+    console.log(maxSum, 'current maxbefore assigment')
+    maxSum = Math.max(max, maxSum)
+    console.log(maxSum, 'current max after assigment')
+    console.log('***************************')
+
+  }
+  return maxSum
+}
+
+let arr = [-2, -3, 4, -1, -2, 1, 5, -1, 1]
+console.log(maxSub(arr)) //7
