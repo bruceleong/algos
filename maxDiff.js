a5ae0efce0e1fa3042d1438b15fe4455186521c8
Bruce Leong <brucewleong@gmail.com>
2018-05-15 18:21:59 -0400

    updated with maxDiff

---
+++ maxDiff.js
@@ -0,0 +1,15 @@
+function maxDiff(arr) {
+  let minSoFar = Infinity, maxDif = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    minSoFar = Math.min(arr[i], minSoFar)
+    // console.log(minSoFar, 'min so far')
+    // console.log(arr[i], 'loop')
+    maxDif = Math.max(arr[i] - minSoFar, maxDif)
+  }
+  return maxDif
+}
+
+let array = [2, 3, 1, 4, 5, 7, 5, 4]
+
+console.log(maxDiff(array))
