d10aa17d209eac5b18d4f454835b892150ab3245
Bruce Leong <brucewleong@gmail.com>
2018-05-17 12:18:07 -0400

    updated algos

---
+++ powerset.js
@@ -0,0 +1,27 @@
+function powerset(array, idx = null) {
+	// let triplets = [[]]
+	// for (const num of array) {
+	// 	let length = triplets.length
+	// 	for (let i = 0; i < length; i++) {
+	// 		triplets.push(triplets[i].concat(num))
+	// 	}
+	// }
+	// return triplets
+	if (idx === null) {
+		idx = array.length - 1
+	}
+	if (idx < 0) {
+		return [[]]
+	}
+
+	let ele = array[idx]
+	let subset = powerset(array, idx - 1)
+	let length = subset.length
+
+		for (let i = 0; i < length; i++) {
+			subset.push(subset[i].concat(ele))
+		}
+	return subset
+}
+
+console.log(powerset([1, 2, 3]))
