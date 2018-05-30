81e5a81855988c601ac590edf8068b6abc040b0a
Bruce Leong <brucewleong@gmail.com>
2018-05-30 11:56:31 -0400

    added numToBinary

---
+++ numToBinaryOnes.js
@@ -0,0 +1,28 @@
+//
+
+const numToBinaryOnes = (num) => {
+  let currentMatchMax = 0;
+  let continuous1s = 0;
+  let binaryNumString = Number(num).toString(2)
+
+  for (let i = 0; i < binaryNumString.length; i++) {
+    if (binaryNumString[i] === '1') {
+      continuous1s += 1
+      if (continuous1s > currentMatchMax) {
+        currentMatchMax = continuous1s
+      }
+    } else {
+      continuous1s = 0
+    }
+  }
+  return currentMatchMax
+}
+
+//testCases
+console.log(numToBinaryOnes('2928018016900296589618184185' )) //12
+console.log(numToBinaryOnes(500)) //5
+console.log(numToBinaryOnes(200)) //2
+console.log(numToBinaryOnes(30)) //4
+console.log(numToBinaryOnes(5)) //1
+console.log(numToBinaryOnes(13)) //2
+console.log(numToBinaryOnes(131)) //2
