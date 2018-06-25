b929104cfd620fdb99660d09e325c4950dfb21b9
Bruce Leong <brucewleong@gmail.com>
2018-06-25 15:36:27 -0400

    added min number of coins

---
+++ minNumOfCoins.js
@@ -0,0 +1,14 @@
+function minNumOfCoins(target, arr) {
+  let changeArr = new Array(target + 1).fill(Infinity)
+  changeArr[0] = 0
+  for (const num of arr) {
+    for (let amount = 0; amount < changeArr.length; amount++) {
+      if (num <= amount) {
+        changeArr[amount] = Math.min(changeArr[amount], changeArr[amount - num] + 1)
+      }
+    }
+  }
+  return changeArr[target] !== Infinity ? changeArr[target] : -1
+}
+
+console.log(minNumOfCoins(7, [1, 5, 10]))
