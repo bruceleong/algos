8c60add351ba8ad66922209c258a8081bf0eed6f
Bruce Leong <brucewleong@gmail.com>
2018-06-24 23:57:27 -0400

    updated subarray sort

--- subArraySort.js
+++ subArraySort.js
@@ -1,18 +1,42 @@
 function subArraySort(array) {
-  let right, left;
-  let tracker = {}
-  for (let i = array.length - 1; i > 0; i--) {
-    if (array[i - 1] > array[i] && array[i] < array[i + 1]) {
-      let outOfPlace = array[i + 1];
-      console.log(outOfPlace)
-      // for (let inner = 0; inner < array.length; inner++) {
-
-      // }
+  let minOutOfOrder = Infinity, maxOutOfOrder = -Infinity
+  for (let i = 0; i < array.length; i++) {
+    const num = array[i]
+    if (isOutOfOrder(i, num, array)) {
+      minOutOfOrder = Math.min(minOutOfOrder, num)
+      maxOutOfOrder = Math.max(maxOutOfOrder, num)
+      console.log(minOutOfOrder, 'current min', maxOutOfOrder, 'current Max')
     }
   }
+  if (minOutOfOrder === Infinity) return [-1, -1]
+  let subArrayLeftIdx = 0;
+  while (minOutOfOrder >= array[subArrayLeftIdx]) {
+    subArrayLeftIdx++;
+  }
+  let subArrayRightIdx = array.length - 1;
+  while (maxOutOfOrder <= array[subArrayRightIdx]) {
+    subArrayRightIdx--;
+  }
+  return [subArrayLeftIdx, subArrayRightIdx]
 }
 
-// let arr = [1, 2, 3, 4]
-let arr = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+function isOutOfOrder(i, num, array) {
+  if (i === 0) return num > array[i + 1]
+  if (i === array.length - 1) return num < array[i - 1]
+  return num > array[i + 1] || num < array[i - 1]
+}
+
+let arr = [1, 3, 2, 4]
+// let arr = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
 
 console.log(subArraySort(arr))
+
+//
+
+// function blah(x) {
+//   if (x > 0) return 'greater than zero'
+//   if (x < 0) return 'less than zero'
+//   return 'zero'
+// }
+
+// console.log(blah(-1))
