267aeda5d71b35d354a096ce393fc5de018a7d4d
Bruce Leong <brucewleong@gmail.com>
2018-07-03 07:16:10 -0400

    added maxSumsNoAdj

---
+++ maxSumsNoAdj.js
@@ -0,0 +1,14 @@
+function maxSumNoAdj(array) {
+  if (!array.length) return 0;
+  if (array.length === 1) return array[0]
+  let second = array[0];
+  let first = Math.max(array[0], array[1])
+  for (let i = 2; i < array.length; i++) {
+    const current = Math.max(first, second + array[i])
+    second = first
+    first = current
+  }
+  return first
+}
+
+console.log(maxSumNoAdj([75, 105, 120, 80, 90, 135]))
