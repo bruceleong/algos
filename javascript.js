9823f68fb48f34aeafd8218e228ec5b04591dfc7
Bruce Leong <brucewleong@gmail.com>
2018-07-06 15:17:41 -0400

    updated with js info

--- javascript.js
+++ javascript.js
@@ -9,4 +9,28 @@ function retirement(retireAge) {
 }
 
 const retirementUS = retirement(66)
-console.log(retirementUS(1990))
+// console.log(retirementUS(1990))
+
+//constructor
+
+function Question(question, anwers, correct) {
+  this.question = question;
+  this.anwers = anwers;
+  this.correct = correct;
+}
+
+// Question.prototype.displayQuestion() {
+//   console.log(this.question);
+//   for (let i = 0; i < )
+// }
+
+let q1 = new Question('is JS, coolest?', ['yes', 'no'], 0);
+
+let q2 = new Question('name of this course teacher', ['me', 'blah'], 2);
+
+let q3 = new Question('best describe coding', ['awesome', 'fun', 'hard'], 0);
+
+let questions = [q1, q2, q3];
+
+let n = Math.floor(Math.random() * questions.length);
+
