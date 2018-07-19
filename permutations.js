7bf5b4a0b819b57b54b8d938d46341b7f89b92c2
Bruce Leong <brucewleong@gmail.com>
2018-07-19 11:54:29 -0400

    updated permutations

--- permutations.js
+++ permutations.js
@@ -1,74 +1,22 @@
 function getPermutations(array) {
-  const perms = [];
-  permHelper(array, [], perms)
-  return perms
+  const perm = [];
+  permutationBuilder(array, [], perm)
+  return perm;
 }
 
-function permHelper(array, currentPermutation, perms) {
-  if (!array.length && currentPermutation.length) {
-    perms.push(currentPermutation)
+function permutationBuilder(array, currentPerms, totalPerms) {
+  if (!array.length && currentPerms.length) {
+    totalPerms.push(currentPerms)
   } else {
     for (let i = 0; i < array.length; i++) {
-      const newArray = array.slice(0, i).concat(array.slice(i + 1))
-      const newPerm = currentPermutation.concat([array[i]])
-      permHelper(newArray, newPerm, perms)
+      let newArray = array.slice(0, i).concat(array.slice(i + 1))
+      let newPerm = currentPerms.concat([array[i]])
+      permutationBuilder(newArray, newPerm, totalPerms)
     }
   }
+  return totalPerms
 }
+
 
 console.log(getPermutations(['a', 'b', 'c']))
 // console.log(getPermutations(['a', 'b', 'c', 'd']))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getPermutations(array) {
-//   const permutations = [];
-//   permutationsHelper(array, [], permutations)
-//   return permutations
-// }
-
-// function permutationsHelper(array, currentPermutation, permutations) {
-
-//   //base case
-//   if (!array.length && currentPermutation.length) {
-//     permutations.push(currentPermutation)
-//   } else {
-//     for (let i = 0; i < array.length; i++) {
-
-//       const newArray = array.slice(0, i).concat(array.slice(i + 1));
-//       const newPermutation = currentPermutation.concat([array[i]])
-//       permutationsHelper(newArray, newPermutation, permutations)
-//     }
-//   }
-// }
