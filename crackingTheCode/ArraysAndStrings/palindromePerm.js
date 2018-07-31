6b041779d44cfc4af3267408e5619d03ddff22e6
Bruce Leong <brucewleong@gmail.com>
2018-07-31 09:35:18 -0400

    added new palindrom perm

--- crackingTheCode/ArraysAndStrings/palindromePerm.js
+++ crackingTheCode/ArraysAndStrings/palindromePerm.js
@@ -1,21 +1,39 @@
+// function palindromePermutation(str) {
+//   let palObj = {};
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === ' ') continue
+//     else if (palObj[str[i]]) palObj[str[i]]++
+//     else palObj[str[i]] = 1
+//   }
+
+//   let pals = [];
+//   for (const letters in palObj) {
+//     if (palObj[letters] % 2 !== 0) {
+//       pals.push(letters)
+//     }
+//   }
+//   console.log(pals, 'pals', palObj)
+//   if (pals.length > 1) return false
+//   else return true
+// }
+
 function palindromePermutation(str) {
-  let palObj = {};
+  const letters = {}
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === ' ') continue
-    else if (palObj[str[i]]) palObj[str[i]]++
-    else palObj[str[i]] = 1
-  }
-
-  let pals = [];
-  for (const letters in palObj) {
-    if (palObj[letters] % 2 !== 0) {
-      pals.push(letters)
+    const letterToCheck = str[i]
+    if (letterToCheck === ' ') {
+      continue;
+    } else if (letters[letterToCheck]) {
+      delete letters[letterToCheck]
+    } else {
+      letters[letterToCheck] = 1
     }
   }
-  console.log(pals, 'pals', palObj)
-  if (pals.length > 1) return false
-  else return true
+  console.log(letters)
+  if (Object.keys(letters).length < 2) return true
+  else return false
 }
 
 console.log(palindromePermutation('taco0 cat'))
