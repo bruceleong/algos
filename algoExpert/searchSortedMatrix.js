129dae5e4f692763bdf5cd170e081a3032917f9c
Bruce Leong <brucewleong@gmail.com>
2018-08-05 19:21:39 -0400

    added search matrix

---
+++ algoExpert/searchSortedMatrix.js
@@ -0,0 +1,28 @@
+function searchMatrix(matrix, target) {
+  let row = 0
+  let column = matrix[0].length - 1
+  while (row < matrix.length && column >= 0) {
+    const potential = matrix[row][column]
+    console.log(potential, '======', [row, column])
+    if (target > potential) {
+      row++
+    } else if (target < potential) {
+      column--
+    } else {
+      return [row, column]
+    }
+  }
+  return [-1, -1]
+}
+
+// console.log(searchMatrix([
+//   [1, 4, 6, 12, 15, 1000],
+//   [2, 5, 19, 30, 32],
+//   [3, 8, 24, 33, 35, 1002]
+// ], 1002))
+
+console.log(searchMatrix([
+  [1, 4, 6, 12, 15, 1000],
+  [5, 6, 19, 30, 32],
+  [3, 8, 24, 33, 35, 1002]
+], 5))
