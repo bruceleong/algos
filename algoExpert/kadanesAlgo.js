50d629c24fb0f6a8ea59c259e3cbbbe975dfa0dd
Bruce Leong <brucewleong@gmail.com>
2018-08-06 23:23:19 -0400

    added kadanes algo

---
+++ algoExpert/kadanesAlgo.js
@@ -0,0 +1,17 @@
+function kadanesAlgo(arr) {
+  //initialize the first element as our check
+  let maxEndingHere = arr[0]
+  let maxSoFar = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    const currentElementToTest = arr[i]
+    //check greatest of - 2 elements = previous + current or current
+    maxEndingHere = Math.max(currentElementToTest, maxEndingHere + currentElementToTest)
+    //check Max of (previous + next  previous) or previous
+    maxSoFar = Math.max(maxSoFar, maxEndingHere)
+  }
+  console.log(maxEndingHere)
+  return maxSoFar
+}
+
+console.log(kadanesAlgo([1, 2, 3, -1]), '-->', 6)
+// console.log(kadanesAlgo([1, -2, 3]), '-->', 2)
